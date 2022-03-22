2db286091a6db4c519cd3564e374676d424bac6e
Nandor Birta <45501377+tripnv@users.noreply.github.com>
2022-03-22 16:01:38 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,111 @@
+#%%
+from re import S
+import streamlit as st
+import pandas as pd
+from datetime import date
+from get_data import calculate_metrics
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+plt.style.use('dark_background')
+#%%
+data = pd.read_csv('data/stocks_data.csv', index_col=[0])
+data.Date = data.Date.apply(lambda x: date.fromisoformat(x))
+
+@st.cache
+def update_data(data, williams_choice, ema_choice):
+    return calculate_metrics(data,
+        will_r_timeperiod=williams_choice,
+        ema_timeperiod=ema_choice)
+#%%
+intro = st.container()
+intro.title("S&P500 metrics")
+    
+# Num tickers/timeframe 
+dataset_info = st.container()
+with dataset_info:
+    st.text(f"The dataset contains daily financial data for {len(data.Symbol.unique())} unique tickers for the timeframe: {data.Date.min()} — {data.Date.max()}")
+    st.text(f"Sample from the dataset:")
+    st.write(data.head(3))
+
+
+# User input and display tickers that satisfy the condition
+parameter_selection = st.container()
+with parameter_selection:
+    st.header('**Select parameters**')
+    williams_choice = st.slider(label = 'Williams R period', min_value=5, max_value = 100, value = 14)
+    ema_choice = st.slider(label = 'EMA for Williams', min_value=3, max_value = 100, value = 21)
+    st.header("Williams %R params")
+    st.text("""
+    Williams %R moves between 0 and -100. 
+    Generally, a reading above -20 is overbought,
+    and a reading below -80 is oversold. However, these can be picked on a use-case basis as well.
+    """)
+    updated_data = update_data(data, williams_choice, ema_choice)
+
+    col1, col2 = st.columns(2)
+    lower_thresh = col1.number_input(label = "Oversold threshold", value = -80, min_value = -100, max_value = 0)
+    upper_thresh = col2.number_input(label = 'Overbought threshold', value = -20, min_value = -100, max_value = 0)
+
+# Filter
+filtered = st.container()
+with filtered:
+    selection_choices = ['below the oversold threshold', 'above the overbought threshold' ]
+    st.header('Tickers of interest')
+    filter_selection = st.selectbox(label = 'Only show tickers:', options=selection_choices)
+    date_selection = st.date_input(
+            label = 'Consider Williams %R on this day',
+            value = data.Date.max(),
+            min_value = data.Date.min(),
+            max_value = data.Date.max())
+
+
+date_filtered_data = updated_data.loc[updated_data.Date == date_selection]
+
+# Filtering results
+if 'below' in filter_selection:
+    # Filter by date
+    thresh_filtered = date_filtered_data.loc[date_filtered_data.WillR < lower_thresh]
+    
+
+else:
+    # Same as above
+    thresh_filtered = date_filtered_data.loc[date_filtered_data.WillR > upper_thresh]
+
+st.write(thresh_filtered[['Date', 'Symbol', 'WillR', 'WillR_EMA', 'Close']])
+
+
+
+
+# Select filtered tickers
+# Detailed exploration and plots
+exploration = st.container()
+with exploration:
+    exploration_choice = st.selectbox(label = "Inspect the following ticker:", options = thresh_filtered.Symbol.unique() )
+    date_col1, date_col2 = st.columns(2)
+    with date_col1:
+        date_lower = st.date_input(
+            label = 'From',
+            value = updated_data.Date.max(),
+            min_value = updated_data.Date.min(),
+            max_value = updated_data.Date.max())
+    with date_col2:
+        date_upper = st.date_input(
+            label = 'To',
+            value = updated_data.Date.max(),
+            min_value = updated_data.Date.min(),
+            max_value = updated_data.Date.max())
+    fig, ax = plt.subplots(figsize = (15, 5))
+    subset = updated_data.loc[(updated_data.Symbol == exploration_choice) & (updated_data.Date > date_lower) & ( updated_data.Date < date_upper)]
+    ax.plot(subset.Date, subset.WillR, label = 'W%R')
+    ax.plot(subset.Date, subset.WillR_EMA, label = 'EMA' )
+    ax.set_title('Williams %R and EMA of Williams %R')
+    plt.legend()
+    plt.xticks(rotation = 45)
+    st.pyplot(fig)
+        
+
+
+# %%
+
+# %%
---
+++ get_data.py
@@ -0,0 +1,172 @@
+# %%
+import os
+import urllib
+from bs4 import BeautifulSoup
+import pandas as pd
+import yfinance as yf 
+import pandas_datareader as dtr
+import datetime 
+import time 
+from tqdm import tqdm
+from copy import deepcopy
+from talib import WILLR
+from talib import EMA
+
+# %%
+HEADERS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Max-Age': '3600',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+    }
+
+
+# %%
+data = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+
+
+# %%
+summary_table = data[0]
+tickers = summary_table['Symbol'].values
+
+# %%
+sample_tickers = tickers[:10]
+
+# %%
+tickers_df = pd.DataFrame(tickers)
+
+
+# %%
+yfparams = {
+    'start':datetime.date.fromisoformat('2020-01-01'),
+    'end':datetime.date.fromisoformat('2022-01-01'),
+    'pause': .1, 
+    'adjust_price':True, 
+    'ret_index':False,
+    'interval':'d',
+    'get_actions': True,
+    'adjust_dividends': True
+}
+
+# %%
+def get_historical_data(tickers,
+    previous_data = None,
+    params = {},
+    timeout = .1):
+    print(f"YF parameters: {params}")
+    collector = []
+    unretrieved_count = 0
+    for ticker in tqdm(tickers):
+        try:
+            df = dtr.get_data_yahoo(symbols = ticker, **params)
+            # df.reset_index(inplace = True)
+            df['Symbol'] = ticker
+            collector.append(df)
+            time.sleep(timeout)
+        except:
+            print(f"Couldn't retieve data for {ticker}")
+            unretrieved_count += 1
+    print(f"# Unretrieved tickers: {unretrieved_count}")
+    collected_data = pd.concat(collector)
+    collected_data.reset_index(inplace=True)
+    collected_data.Date = collected_data.Date.apply(lambda x: x.date())
+    if previous_data is not None:
+        if not previous_data.empty:
+            updated_data = pd.concat([collected_data, previous_data], axis=0)
+            return collected_data, previous_data, updated_data
+    else:
+        return collected_data
+
+# %%
+# def get_last_weekday(given_date):
+#     given_date -= datetime.timedelta(days=1)
+#     while given_date.weekday() > 4: # Mon-Fri are 0-4
+#         given_date -= datetime.timedelta(days=1)
+#     return given_date
+
+# %%
+# def get_daily_update(tickers,
+#                     previous_data = None,
+#                     date_string: str = None,
+#                     yfparams: dict = None):
+#     """ If date_string = None --> get today's """
+#     params = deepcopy(yfparams)
+    
+#     if date_string is None:
+#         last_weekday = get_last_weekday(datetime.datetime.today() - datetime.timedelta(days = 1))
+#         params['end'] = last_weekday
+#         params['start'] = last_weekday
+#     else:
+#         params['end'] = datetime.date.fromisoformat(date_string)
+#         params['start'] = datetime.date.fromisoformat(date_string)
+
+
+#     # When to update?
+#     if params['start'].isoweekday() not in range(1,6):
+#         print('The selected day is a weekend...')
+#         raise AttributeError
+    
+
+#     daily_update = get_historical_data(tickers = tickers, previous_data = None, yfparams = params, timeout=.1)   
+#     if previous_data is not None:
+#         return pd.concat([previous_data, daily_update])
+#     else:
+#         return daily_update
+
+
+
+# %%
+
+# %%
+def update_dataframe(tickers, previous_data: pd.DataFrame, params:dict, timeout = 1):
+    """ Given a dataframe, update it to include everything until today """
+
+    start = datetime.datetime.strptime(previous_data.Date.max(), '%Y-%m-%d') + datetime.timedelta(days = 1)
+    end = datetime.date.today() - datetime.timedelta(days = 1) # Yesterday
+    params['start'] = start
+    params['end'] = end
+    
+    new_data = get_historical_data(tickers = tickers, previous_data = previous_data, params=params, timeout=timeout)
+    return new_data
+
+
+
+
+# %%
+def calculate_metrics(updated_df, will_r_timeperiod = 21, ema_timeperiod = 13):
+    tickers = updated_df.Symbol.unique()
+
+    updated_df.sort_values(by = ['Symbol', 'Date'], ascending=[True, True], inplace = True)
+    indexed_df = updated_df.reset_index()
+    
+    # Set empty columns for the large dataset
+    indexed_df[['WillR','WillR_EMA']] = None
+
+    for ticker in tickers:
+        subset_of_interest = indexed_df.loc[indexed_df['Symbol'] == ticker]
+        subset_of_interest.loc[subset_of_interest.index, 'WillR'] = WILLR(subset_of_interest.High, subset_of_interest.Low, subset_of_interest.Close, timeperiod = will_r_timeperiod)
+        subset_of_interest.loc[subset_of_interest.index, 'WillR_EMA'] = EMA(subset_of_interest.WillR, timeperiod = ema_timeperiod)
+        
+        indexed_df.loc[subset_of_interest.index, 'WillR'] = subset_of_interest['WillR']
+        indexed_df.loc[subset_of_interest.index, 'WillR_EMA'] = subset_of_interest['WillR_EMA']
+    indexed_df.drop(columns = ['index'], inplace = True)
+    return indexed_df
+
+# %%
+
+
+
+if __name__ == "__main__":
+    data_path = 'data/stocks_data.csv'
+    if os.path.exists(data_path):
+        data = pd.read_csv(data_path, index_col=[0])
+        c, p, u = update_dataframe(tickers, previous_data=data, params = yfparams, timeout=0.3)
+        u.to_csv(data_path)
+    else:
+        data = get_historical_data(tickers = tickers, previous_data=None, params = yfparams)
+        data.to_csv(data_path)
+
+#%%
+
+# %%
