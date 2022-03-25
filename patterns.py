f03118f8630389c65d8fee02520881e7e89d5074
tripnv <nandibirta@gmail.com>
2022-03-25 14:06:24 +0200

    experiments w/ ws

--- patterns.py
+++ patterns.py
@@ -1,11 +1,13 @@
 #%%
-from curses import window
 from metrics import calculate_metrics, read_all_tickers
 import pandas as pd 
-from scipy.signal import find_peaks, find
+from scipy.signal import find_peaks
 import matplotlib.pyplot as plt
+import datetime
+import pandas_datareader as dtr
 # %%
-data = read_all_tickers('tickers copy')
+data = read_all_tickers('tickers')
+#%%
 # %%
 subset = data.loc[data.Symbol == 'MSFT']
 # %%
@@ -45,14 +47,71 @@ db_count
 
 # %%
 mod_db = [ db*.97 for db in double_bottoms] 
-# %%
-mod_db
+
 # %%
 plt.figure(figsize=(50,5))
 plt.plot(subset.index, subset.Close)
-plt.scatter(x = peaks, y = subset.Close.iloc[peaks], alpha = .3, color = 'r')
-plt.scatter(x = bottoms, y = subset.Close.iloc[bottoms], alpha = .3, color = 'g')
+plt.scatter(x = peaks, y = subset.Close.iloc[peaks], alpha = .9, color = 'g')
+plt.scatter(x = bottoms, y = subset.Close.iloc[bottoms], alpha = .9, color = 'r')
+i = 0
 for db in mod_db:
-    plt.plot(db.index, db.values, color = 'purple', alpha = .5)
-    plt.axvspan(db.index[0], db.index[-1], color = 'pink', alpha = .5)
+    plt.plot(db.index, db.values, color = 'purple',linestyle = '-', alpha = .5)
+    plt.axvspan(db.index[0], db.index[-1], color = 'lightgray', alpha = .5) 
+    plt.text(x = db.index[0], y = db.values[0] * .98, s = str(i))
+    i+=1
+ # %%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+yf_params = {
+    'start':datetime.date.today() - datetime.timedelta(days = 1),
+    'end':datetime.date.today(),
+    'pause': .2, 
+    'adjust_price':True, 
+    'ret_index':False,
+    'interval':'d',
+    'get_actions': True,
+    'adjust_dividends': True
+}
+tickers = [s.split('.')[0] for s in os.listdir('data/tickers')]
+#%%
+
 # %%
+x = dtr.get_data_yahoo(symbols = tickers, **yf_params)
+# %%
+x.T
+# %%
+x.columns
+# %%
+ 
